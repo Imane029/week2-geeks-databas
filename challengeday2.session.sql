6efda04efc91984599cb0b47d3ce7760d8995c78
Imane029 <imaneryad29@gmail.com>
2025-08-22 12:00:45 +0100

    Initial commit for SQL puzzle

---
+++ challengeday2.session.sql
@@ -0,0 +1,44 @@
+CREATE TABLE FirstTab (
+    id integer, 
+    name VARCHAR(10)
+);
+
+CREATE TABLE SecondTab (
+    id integer
+);
+
+INSERT INTO FirstTab VALUES
+(5,'Pawan'),
+(6,'Sharlee'),
+(7,'Krish'),
+(NULL,'Avtaar');
+
+INSERT INTO SecondTab VALUES
+(5),
+(NULL);
+
+select * from FirstTab;
+
+-- Question 1
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );
+
+-- Question 2
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 );
+
+-- Question 3
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
+
+-- Question 4
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );
+
+
+
+
+
+
+
+
