e0e0bdf13a4ff139f6e87c0daefff2ddb0d1b9c2
Imane029 <imaneryad29@gmail.com>
2025-08-25 00:08:27 +0100

    fix issues

--- day2.sql
+++
@@ -1,9 +0,0 @@
---SELECT * FROM items ORDER BY price ASC;
---SELECT * FROM items WHERE price >= 80 ORDER BY price DESC;
---SELECT first_name, last_name FROM customers ORDER BY first_name ASC LIMIT 3;
---SELECT last_name FROM customers ORDER BY last_name DESC;
-SELECT * FROM items;
-SELECT * FROM customers;
-
--- Solution pour l'exercice "Items and customers".
--- Ce script contient les requêtes pour répondre aux quatre instructions du défi.
