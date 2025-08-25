d3cb0fa3b20c35762f709015a7522cd5d0655cb8
Imane029 <imaneryad29@gmail.com>
2025-08-25 16:39:19 +0100

    challeng4

---
+++ challenge data.sql
@@ -0,0 +1,11 @@
+
+SELECT * FROM actors;
+
+-- SELECT COUNT(*) AS total_actors;
+
+-- Solution pour le défi quotidien "Actors".
+-- Cette requête affiche la table complète après l'insertion.
+-- La requête pour compter le nombre d'acteurs est incluse mais commentée,
+-- pour démonter la première étape du défi.
+
+
---
+++ challengeday2.sql
@@ -0,0 +1,6 @@
+--SELECT * FROM FirstTab;
+
+TRUNCATE TABLE FirstTab;
+
+
+
